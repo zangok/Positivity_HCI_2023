90c9cc4cab297314c6747a01fe6665db55962442
zangok <81659243+zangok@users.noreply.github.com>
2023-10-30 14:39:32 -0500

    Project starts now

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
 }
-
 android {
     namespace = "com.example.positivity_hci_2023"
     compileSdk = 34
@@ -39,8 +38,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata:2.6.2")
+    //implementation("androidx.lifecycle:lifecycle-view-model-ktx:2.6.2")
     implementation("androidx.navigation:navigation-fragment:2.7.4")
     implementation("androidx.navigation:navigation-ui:2.7.4")
     testImplementation("junit:junit:4.13.2")
