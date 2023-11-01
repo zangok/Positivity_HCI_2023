9b47e06cdeac50787e157dc4b3196b702fca628a
zangok <81659243+zangok@users.noreply.github.com>
2023-11-01 11:19:01 -0500

    Clicking notifications opens main activity

--- app/src/main/java/com/example/positivity_hci_2023/Notifications.java
+++ app/src/main/java/com/example/positivity_hci_2023/Notifications.java
@@ -3,7 +3,9 @@ package com.example.positivity_hci_2023;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
@@ -12,24 +14,27 @@ public class Notifications {
     private String channelId = "my_channel_1";
     private CharSequence channelName = "My Channel";
     public Notifications(Context context) {
-        // Define a channel ID and name
-
-
-// Create the notification channel
+        //create notification channel
         NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT);
         NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
         notificationManager.createNotificationChannel(channel);
         CharSequence textTitle = "Positivity";
-
         CharSequence textContent = "You've been on your phone for too long";
+        //clicking opens app
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE);
+        //Create notification
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelId)
                 .setSmallIcon(R.drawable.baseline_arrow_back_24)
                 .setContentTitle(textTitle)
                 .setContentText(textContent)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             notificationManager.createNotificationChannel(channel);
         }
+        //Display notification
         notificationManager.notify(0, builder.build());
     }
     private void createNotificationChannel() {
