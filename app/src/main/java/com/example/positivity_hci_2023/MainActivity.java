99da6ec3cbc1e976a0fe5c635e5aa1a9e6e21baa
fisherstick <jason.fisher@utdallas.edu>
2023-11-09 22:55:42 -0600

    Added functionality to the Resources page to include 2 resources similar to Figma and Marvel.

--- app/src/main/java/com/example/positivity_hci_2023/MainActivity.java
+++ app/src/main/java/com/example/positivity_hci_2023/MainActivity.java
@@ -1,16 +1,14 @@
 package com.example.positivity_hci_2023;
 
 import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.example.positivity_hci_2023.databinding.ActivityMainBinding;
+import com.example.positivity_hci_2023.ui.resources.ResourcesFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,11 +30,27 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-        getSupportActionBar().hide();
-        getSupportActionBar().setTitle("Positivity");
-        //Notifications a = new Notifications(this);
 
-
+        // Hide the action bar if it's not needed
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+            getSupportActionBar().setTitle("Positivity");
+        }
     }
 
+    @Override
+    public void onBackPressed() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        // Use the ID from the navigation graph
+        if (navController.getCurrentDestination().getId() == R.id.navigation_resources) {
+            ResourcesFragment resourcesFragment = (ResourcesFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.navigation_resources); // Use the ID from the navigation graph
+            if (resourcesFragment != null && resourcesFragment.canGoBack()) {
+                // If canGoBack returns true, back action was handled by the fragment
+                return;
+            }
+        }
+        // If not handled by ResourcesFragment, call the super method to handle default back action
+        super.onBackPressed();
+    }
 }
--- app/src/main/java/com/example/positivity_hci_2023/ui/resources/ResourcesFragment.java
+++ app/src/main/java/com/example/positivity_hci_2023/ui/resources/ResourcesFragment.java
@@ -4,14 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
+import com.example.positivity_hci_2023.R;
 import com.example.positivity_hci_2023.databinding.FragmentResourcesBinding;
-import com.example.positivity_hci_2023.ui.resources.ResourcesViewModel;
 
 public class ResourcesFragment extends Fragment {
 
@@ -19,20 +19,58 @@ public class ResourcesFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        ResourcesViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(ResourcesViewModel.class);
-
         binding = FragmentResourcesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textResources;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        // Initialize the WebView
+        WebView webView = binding.webviewResources;
+        webView.getSettings().setJavaScriptEnabled(true); // Enable JavaScript if needed
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                // Show the WebView when the page is finished loading
+                webView.setVisibility(View.VISIBLE);
+            }
+        });
+
+        // Buttons to load the local HTML files
+        Button buttonWikipedia = root.findViewById(R.id.buttonWikipedia);
+        buttonWikipedia.setOnClickListener(v -> {
+            webView.loadUrl("file:///android_asset/problematic_smartphone_use_wikipedia.html");
+            toggleWebViewVisibility(true);
+        });
+
+        Button buttonSamhsa = root.findViewById(R.id.buttonSamhsa);
+        buttonSamhsa.setOnClickListener(v -> {
+            webView.loadUrl("file:///android_asset/samhsa_national_helpline.html");
+            toggleWebViewVisibility(true);
+        });
+
         return root;
+    }
+
+    private void toggleWebViewVisibility(boolean show) {
+        // If true, show the WebView
+        binding.webviewResources.setVisibility(show ? View.VISIBLE : View.GONE);
+        // If false, show the buttons
+        binding.buttonWikipedia.setVisibility(show ? View.GONE : View.VISIBLE);
+        binding.buttonSamhsa.setVisibility(show ? View.GONE : View.VISIBLE);
+    }
+
+    public boolean canGoBack() {
+        if (binding.webviewResources.getVisibility() == View.VISIBLE && binding.webviewResources.canGoBack()) {
+            binding.webviewResources.goBack();
+            return true;
+        }
+        return false;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        if (binding.webviewResources != null) {
+            binding.webviewResources.destroy();
+        }
         binding = null;
     }
 }
